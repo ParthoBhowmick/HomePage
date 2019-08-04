165701be343b44829c1f67bae38da514c8c3f92a
ParthoBhowmick <parthopritomcv@gmail.com>
2019-08-04 18:26:24 +0600

    day 2..

--- app/src/main/java/com/example/homepagedemo/MainActivity.java
+++ app/src/main/java/com/example/homepagedemo/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.homepagedemo;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +26,32 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        ImageView img = findViewById(R.id.navIcon);
 
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-////
-////        drawer.addDrawerListener(toggle);
-////        toggle.syncState();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        toggle.setDrawerIndicatorEnabled(false);
+//        toolbar.setNavigationIcon(R.drawable.ic_menu);
+//
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawer.openDrawer(GravityCompat.START);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
