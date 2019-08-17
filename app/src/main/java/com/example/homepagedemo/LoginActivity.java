3f31d74e540be0312c7b2ffec690db3cd8d04e0c
ParthoBhowmick <parthopritomcv@gmail.com>
2019-08-18 04:05:58 +0600

    fb login halfway

--- app/src/main/java/com/example/homepagedemo/LoginActivity.java
+++ app/src/main/java/com/example/homepagedemo/LoginActivity.java
@@ -4,12 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    CallbackManager mCallbackManager;
+    LoginButton login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.dummy);
+
+        login = findViewById(R.id.facebookLogin);
+        mCallbackManager = CallbackManager.Factory.create();
+
+        // Callback registration
+        login.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+
     }
 }
