c4f69d9c2725c53338be5d18b69d279a29270f5c
ParthoBhowmick <parthopritomcv@gmail.com>
2019-08-18 18:41:40 +0600

    solving a minor bug

--- app/src/main/java/com/example/homepagedemo/MainActivity.java
+++ app/src/main/java/com/example/homepagedemo/MainActivity.java
@@ -8,16 +8,14 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.awt.MenuItem;
 import java.util.ArrayList;
+
+import javax.swing.text.View;
+import javax.swing.text.html.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -77,53 +75,72 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<Model> models = new ArrayList<>();
 
-        Model model1_1 = new Model("Carmelita Badura", 1 );
-        Model model1_1_1 = new Model("Elisha Pickert", 2);
-        Model model1_1_2 = new Model("Patricia Netherton", 2);
-        Model model1_1_3 = new Model("Christeen Petrey", 2);
-        Model model1_1_4 = new Model("Lolita Moreman", 2);
+        Model model1_3 = new Model("Shop", 1);
+
+        Model model1_1 = new Model("Flash Discount Offer", 1 );
+        Model model1_1_1 = new Model("20% Discount", 2);
+        Model model1_1_2 = new Model("30% Discount", 2);
+        Model model1_1_3 = new Model("50% Discount", 2);
+
         model1_1.models.add(model1_1_1);
         model1_1.models.add(model1_1_2);
         model1_1.models.add(model1_1_3);
-        model1_1.models.add(model1_1_4);
 
-        Model model1_2 = new Model("Manuela Kass", 1);
-        Model model1_2_1 = new Model("Roseanna Branham", 2);
+        Model model1_2 = new Model("Kids & Mom", 1);
+        Model model1_2_1 = new Model("Infants", 2);
 
-        Model model1_2_1_1 = new Model("Dennise Lasso", 3);
-        Model model1_2_1_2 = new Model("Sabrina Shively", 3);
-        Model model1_2_1_3 = new Model("Jin Haecker", 3);
-        Model model1_2_1_4 = new Model("Season Parrett  ", 3);
+
+
+        Model model1_2_1_1 = new Model("Baby Walker", 3);
+        Model model1_2_1_2 = new Model("Baby Feeder", 3);
+        Model model1_2_1_3 = new Model("Bottle Cleaning Brush", 3);
+        Model model1_2_1_4 = new Model("Cap", 3);
+        Model model1_2_1_5 = new Model("Dress", 3);
         model1_2_1.models.add(model1_2_1_1);
         model1_2_1.models.add(model1_2_1_2);
         model1_2_1.models.add(model1_2_1_3);
         model1_2_1.models.add(model1_2_1_4);
+        model1_2_1.models.add(model1_2_1_5);
 
-        Model model1_2_2 = new Model("Vicky Parkhurst", 2);
-        Model model1_2_3 = new Model("Taisha Dragoo", 2);
-        Model model1_2_4 = new Model("Abbey Ballance", 2);
+
+        Model model1_2_2 = new Model("Kids", 2);
+        Model model1_2_2_1 = new Model("Baby Gear", 3);
+        Model model1_2_2_2 = new Model("Baby Hair Decoration", 3);
+        Model model1_2_2_3 = new Model("Baby Socks", 3);
+        Model model1_2_2_4 = new Model("Children Cycle", 3);
+
+        model1_2_2.models.add(model1_2_2_1);
+        model1_2_2.models.add(model1_2_2_2);
+        model1_2_2.models.add(model1_2_2_3);
+        model1_2_2.models.add(model1_2_2_4);
+
         model1_2.models.add(model1_2_1);
         model1_2.models.add(model1_2_2);
-        model1_2.models.add(model1_2_3);
-        model1_2.models.add(model1_2_4);
 
 
+        Model model1_4 = new Model("Home Decor", 1);
+        Model model1_4_1 = new Model("Home Textile", 2);
+        Model model1_4_1_1 = new Model("Floor Mat", 3);
+        Model model1_4_1_2 = new Model("Bed Sheet", 3);
+        Model model1_4_1_3 = new Model("Pillow & Cushion", 3);
+        Model model1_4_1_4 = new Model("Blanket", 3);
+        Model model1_4_1_5 = new Model("Curtains", 3);
 
-        Model model1_3 = new Model("Arlinda Fogal", 1);
+        model1_4_1.models.add(model1_4_1_1);
+        model1_4_1.models.add(model1_4_1_2);
+        model1_4_1.models.add(model1_4_1_3);
+        model1_4_1.models.add(model1_4_1_4);
+        model1_4_1.models.add(model1_4_1_5);
 
-        Model model1_4 = new Model("Stephen Cabe", 1);
-        Model model1_4_1 = new Model("Cherilyn Lehn", 2);
-        Model model1_4_2 = new Model("Lashay Baumer", 2);
-        Model model1_4_3 = new Model("Abbie Kilmer", 2);
-        Model model1_4_4 = new Model("Clinton Boyers", 2);
+        Model model1_4_2 = new Model("Furniture", 2);
+        Model model1_4_3 = new Model("Wall Decor", 2);
         model1_4.models.add(model1_4_1);
         model1_4.models.add(model1_4_2);
         model1_4.models.add(model1_4_3);
-        model1_4.models.add(model1_4_4);
 
+        models.add(model1_3);
         models.add(model1_1);
         models.add(model1_2);
-        models.add(model1_3);
         models.add(model1_4);
 
 
